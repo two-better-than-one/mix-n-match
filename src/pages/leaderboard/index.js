48e5c1af2dddf0d9b9c321b16589ba6660b74b6f
seattle-mini <sl8305@hotmail.com>
2021-03-16 18:38:39 -0700

    leaderboard page buttons navigate to their respective links

--- src/pages/leaderboard/index.js
+++ src/pages/leaderboard/index.js
@@ -1,11 +1,51 @@
 import './leaderboard.css';
+import {
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import SelectDifficulty from '../select-difficulty';
+import Credits from '../credits';
+
 
 function Leaderboard() {
-    return (
-      <div className='leaderboardContainer'>
+  return (
+    <div className='leaderboardContainer'>
+      
+      <div className='chart'>
         <h2>Top Scores!</h2>
+
       </div>
-    );
-  }
+
+      <div className='leaderboardOptions'>    
+        <ul>
+          <li>
+            <Link to={`/`}>Main Menu</Link>
+          </li>
+          <li>
+            <Link to={`/selectdifficulty`}>Play a game</Link>
+          </li>
+          <li>
+            <Link to={`/credits`}>Credits</Link>
+          </li>
+        </ul>
+
+      <Switch>
+        <Route exact path="/">
+        </Route>
+        <Route path="/selectdifficulty">
+          <SelectDifficulty />
+        </Route>
+        <Route path="/credits">
+          <Credits />
+        </Route>
+      </Switch>
+
+      </div>
+
+    </div>
+  );
+}
 
 export default Leaderboard;
