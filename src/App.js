8ff77d75a0e0d5527943ae1dd92dd15115d535f5
seattle-mini <sl8305@hotmail.com>
2021-03-26 16:49:50 -0700

    app.js was turned into a class component, pulled in from credits and tutorial

--- src/App.js
+++ src/App.js
@@ -17,81 +17,86 @@ import './App.css';
 
 class App extends Component {
 
+  // state
   constructor(props) {
     super(props);
-    // toggle for hidding the first options
+
     this.state = {isHidden: false};
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+    
+  handleClick() {
+    this.setState(state => ({
+      isHidden: !state.isHidden
+    }));
   }
 
-  render () {
-
-  var toggle = (e) => {
-    this.setState(({ isHidden }) => ({ isHidden: !isHidden }));
-  };
-
-  return (
-  
-
-    // Landing page
-    <div className="App">
 
 
-      <Router>
-      <div className='center'>
-        <h1>Mix N Match</h1>
+  render() {
+    return(
+      <div className="App">
 
-        <nav className={this.state.isHidden ? "hidden" : ""}>
-          <ul  className={this.state.isHidden ? "hidden" : ""}>
-            <li  className={this.state.isHidden ? "hidden" : ""}>
-              <Link 
-                to="/tutorial" 
-                className={this.state.isHidden ? "hidden" : ""} 
-                onClick={toggle}>
-                  Tutorial
-              </Link>
-            </li>
-            <li>
-              <Link 
-                to="/selectdifficulty" 
-                className={this.state.isHidden ? "hidden" : ""} 
-                onClick={toggle}>
-                  Start
-              </Link>
-            </li>
-          </ul>
+        <Router>
+          <div className='center'>
+            <h1 className={this.state.isHidden ? 'hidden' : ''}>
+              Mix N Match
+            </h1>
 
-          <ul>
-            <li>
-              <Link
-                to="/leaderboard" 
-                className={this.state.isHidden ? "hidden" : ""} 
-                onClick={toggle} >
-                  Leaderboard
-              </Link>
-            </li>
-            <li>
-              <Link 
-                to="/credits"
-                className={this.state.isHidden ? "hidden" : ""} 
-                onClick={toggle}>
-                  Credits
-              </Link>
-            </li>
-          </ul>
-        </nav>
+            <nav className={this.state.isHidden ? 'hidden' : ''}>
+              <ul className={this.state.isHidden ? 'hidden' : ''}>
+                <li className={this.state.isHidden ? 'hidden' : ''}>
+                  <Link 
+                    to="/tutorial" 
+                    className={this.state.isHidden ? 'hidden' : ''}
+                    onClick={this.handleClick}>
+                    Tutorial
+                  </Link>
+                </li>
+                <li>
+                  <Link 
+                    to="/selectdifficulty"
+                    className={this.state.isHidden ? 'hidden' : ''}
+                    onClick={this.handleClick}>
+                    Start
+                  </Link>
+                </li>
 
-        <Switch>
-          <Route path="/tutorial" component={Tutorial} />
-          <Route path="/selectdifficulty" component={SelectDifficulty} />
-          <Route path="/leaderboard" component={Leaderboard} />
-          <Route path="/credits" component={Credits} />
-        </Switch>
-        
+              </ul>
+
+              <ul className={this.state.isHidden ? 'hidden' : ''}>
+                <li className={this.state.isHidden ? 'hidden' : ''}>
+                  <Link 
+                    to="/selectdifficulty"
+                    className={this.state.isHidden ? 'hidden' : ''}
+                    onClick={this.handleClick}>
+                    Leaderboard
+                  </Link>
+                </li>
+                <li className={this.state.isHidden ? 'hidden' : ''}>
+                  <Link 
+                    to="/selectdifficulty"
+                    className={this.state.isHidden ? 'hidden' : ''}
+                    onClick={this.handleClick}>
+                    Credits
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+
+            <Switch>
+              <Route path="/tutorial" component={Tutorial} />
+              <Route path="/selectdifficulty" component={SelectDifficulty} />
+              <Route path="/leaderboard" component={Leaderboard} />
+              <Route path="/credits" component={Credits} />
+            </Switch>
+          </div>
+        </Router>
       </div>
-    </Router>
-  </div>
-    
-  );
+
+    );
   }
 }
 
--- src/pages/credits/index.js
+++ src/pages/credits/index.js
@@ -1,11 +1,68 @@
+import React from 'react';
+import '../../style/reset.css';
 import './credits.css';
+import marino from '../../images/backgrounds/marino.jpg';
+import shad from '../../images/backgrounds/shad.jpg';
 
 function Credits() {
-    return (
-      <div className='creditsContainer'>
-        <h2>Credits</h2>
+  return (
+    <div className='container'>
+
+      <div className='title'>
+        <p className='title'>Mix n Match</p>
+        <p className='sub-title'>Brought to you by:</p>
       </div>
-    );
-  }
+
+      <div className='main-body'>
+
+        <div className='shad'>
+          <div className='card-container'>
+            <div className='card'>
+              <img src={shad} width='125' alt='Shad Lee' title='Shad Lee' className='side'></img>
+              <div className="side back">info about Shad will go here</div>
+            </div>
+          </div>
+
+          <div className='shad-links'>
+            <button className='s-portfolio'><a href='https://sl8305.github.io/efficient-portfolio/'>Portfolio</a></button>
+            <button className='s-linked'><a href='https://www.linkedin.com/in/shad-lee/'>LinkedIn</a></button>
+            <button className='s-github'><a href='https://github.com/sl8305'>GitHub</a></button>
+          </div>
+
+
+        </div>
+
+        <div className='marino'>
+          <div className='card-container'>
+            <div className='card'>
+              <img src={marino} alt='Mariño Carranza' title='Mariño Carranza' className='side'></img>
+              <div className="side back">Guatemalan, husband, father, Front End Dev, soccer player, baseball fan. Order varies depending on needs.</div>
+            </div>
+          </div>
+
+          <div className='marino-links'>
+            <button className='m-portfolio'><a href='https://pmcarranza.github.io/MarinoCarranza/'>Portfolio</a></button>
+            <button className='m-linked'><a href='https://www.linkedin.com/in/marino-carranza-160a99177/'>LinkedIn</a></button>
+            <button className='m-github'><a href='https://github.com/PMCarranza'>GitHub</a></button>
+          </div>
+
+
+        </div>
+
+        <div className='options'>
+          <button className='menu'><a href='../../App.js'>Main menu</a></button>
+          <button className='play'><a href='../select-difficulty'>Play a game</a></button>
+          <button className='board'><a href='../leaderboard'>Leaderboard</a></button>
+        </div>
+
+      </div>
+
+      <div className='empty'>
+      </div>
+
+    </div>
+
+  );
+}
 
 export default Credits;
